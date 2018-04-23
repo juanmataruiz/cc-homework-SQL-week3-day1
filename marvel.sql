6d39b009552afa99f28d309df8c2415e4e2ef0bf
Juan <juanmataruiz84@gmail.com>
2018-04-23 21:53:34 +0100

    clean code commit

--- marvel.sql
+++ marvel.sql
@@ -58,40 +58,258 @@ INSERT INTO movies (title, year, show_time) VALUES ('Doctor Strange', 2016, '17:
 --1. Return ALL the data in the 'movies' table.
 SELECT * FROM movies;
 
+id |                title                | year | show_time
+----+-------------------------------------+------+-----------
+  1 | Iron Man                            | 2008 | 19:55
+  2 | The Incredible Hulk                 | 2008 | 18:40
+  3 | Iron Man 2                          | 2010 | 21:55
+  4 | Thor                                | 2011 | 16:35
+  5 | Captain America: The First Avenger  | 2011 | 13:25
+  6 | Avengers Assemble                   | 2012 | 22:05
+  7 | Iron Man 3                          | 2013 | 23:10
+  8 | Thor: The Dark World                | 2013 | 12:25
+  9 | Batman Begins                       | 2005 | 14:00
+ 10 | Captain America: The Winter Soldier | 2014 | 14:45
+ 11 | Guardians of the Galaxy             | 2014 | 18:55
+ 12 | Avengers: Age of Ultron             | 2015 | 20:40
+ 13 | Ant-Man                             | 2015 | 13:50
+ 14 | Captain America: Civil War          | 2016 | 21:30
+ 15 | Doctor Strange                      | 2016 | 17:55
+(15 rows)
+
 --2. Return ONLY the name column from the 'people' table
 SELECT name FROM people;
+
+name
+------------------------
+Euan Bell
+Pauline Cairns
+Yang Chen
+Ruairidh Grass
+Hannah Green
+Hamish Hoad
+Andrew Laughlin
+Jo Malo
+Duncan Marjoribanks
+Juan Mata Ruiz
+Paul McPhail Stevenson
+Sarah Murphy
+Katy Preston
+Richard Ramson
+Iain Rogerson
+Davinda Sanders
+Joe Stafford
+Daniel Stewart
+Luis Tejero
+Colin Tindle
+Rachel Westwater
+Andrew Smith
+Zsolt Podoba-Szalai
+(23 rows)
 
 --3.Oops! Someone at CodeClan spelled Davina's name wrong! Change it to reflect the proper spelling (change 'Davinda Sanders' to 'Davina Sanders').
 UPDATE people SET name = 'Davina Sanders' WHERE name = 'Davinda Sanders';
 --SELECT * FROM people;
 
+id |          name
+----+------------------------
+  1 | Euan Bell
+  2 | Pauline Cairns
+  3 | Yang Chen
+  4 | Ruairidh Grass
+  5 | Hannah Green
+  6 | Hamish Hoad
+  7 | Andrew Laughlin
+  8 | Jo Malo
+  9 | Duncan Marjoribanks
+ 10 | Juan Mata Ruiz
+ 11 | Paul McPhail Stevenson
+ 12 | Sarah Murphy
+ 13 | Katy Preston
+ 14 | Richard Ramson
+ 15 | Iain Rogerson
+ 17 | Joe Stafford
+ 18 | Daniel Stewart
+ 19 | Luis Tejero
+ 20 | Colin Tindle
+ 21 | Rachel Westwater
+ 22 | Andrew Smith
+ 23 | Zsolt Podoba-Szalai
+ 16 | Davina Sanders
+(23 rows)
+
 --4. Return ONLY your name from the 'people' table.
 SELECT name FROM people WHERE name = 'Juan Mata Ruiz';
+
+name
+----------------
+Juan Mata Ruiz
+(1 row)
 
 --5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 DELETE FROM movies WHERE title = 'Batman Begins';
 --SELECT * FROM movies;
 
+id |                title                | year | show_time
+----+-------------------------------------+------+-----------
+  1 | Iron Man                            | 2008 | 19:55
+  2 | The Incredible Hulk                 | 2008 | 18:40
+  3 | Iron Man 2                          | 2010 | 21:55
+  4 | Thor                                | 2011 | 16:35
+  5 | Captain America: The First Avenger  | 2011 | 13:25
+  6 | Avengers Assemble                   | 2012 | 22:05
+  7 | Iron Man 3                          | 2013 | 23:10
+  8 | Thor: The Dark World                | 2013 | 12:25
+ 10 | Captain America: The Winter Soldier | 2014 | 14:45
+ 11 | Guardians of the Galaxy             | 2014 | 18:55
+ 12 | Avengers: Age of Ultron             | 2015 | 20:40
+ 13 | Ant-Man                             | 2015 | 13:50
+ 14 | Captain America: Civil War          | 2016 | 21:30
+ 15 | Doctor Strange                      | 2016 | 17:55
+(14 rows)
+
+
 --6. Create a new entry in the 'people' table with the name of one of the instructors.
 INSERT INTO people (name) VALUES ('Sandy');
 --SELECT name FROM people;
 
+name
+------------------------
+Euan Bell
+Pauline Cairns
+Yang Chen
+Ruairidh Grass
+Hannah Green
+Hamish Hoad
+Andrew Laughlin
+Jo Malo
+Duncan Marjoribanks
+Juan Mata Ruiz
+Paul McPhail Stevenson
+Sarah Murphy
+Katy Preston
+Richard Ramson
+Iain Rogerson
+Davinda Sanders
+Joe Stafford
+Daniel Stewart
+Luis Tejero
+Colin Tindle
+Rachel Westwater
+Andrew Smith
+Zsolt Podoba-Szalai
+Sandy
+(24 rows)
+
 --7. Zsolt, has decided to hijack our movie evening, Remove him from the table of people.
 DELETE FROM people WHERE name = 'Zsolt Podoba-Szalai';
 --SELECT * FROM people
+
+id |          name
+----+------------------------
+ 1 | Euan Bell
+ 2 | Pauline Cairns
+ 3 | Yang Chen
+ 4 | Ruairidh Grass
+ 5 | Hannah Green
+ 6 | Hamish Hoad
+ 7 | Andrew Laughlin
+ 8 | Jo Malo
+ 9 | Duncan Marjoribanks
+10 | Juan Mata Ruiz
+11 | Paul McPhail Stevenson
+12 | Sarah Murphy
+13 | Katy Preston
+14 | Richard Ramson
+15 | Iain Rogerson
+16 | Davinda Sanders
+17 | Joe Stafford
+18 | Daniel Stewart
+19 | Luis Tejero
+20 | Colin Tindle
+21 | Rachel Westwater
+22 | Andrew Smith
+(22 rows)
 
 --8. Somehow the list of people includes two people named 'Andrew'. Change these entries to the proper names ('Jeff 4', 'Jeff 5')
 UPDATE people SET name = 'Jeff 4' WHERE name = 'Andrew Laughlin';
 UPDATE people SET name = 'Jeff 5' WHERE name = 'Andrew Smith';
 --SELECT name FROM people;
 
+name
+------------------------
+Euan Bell
+Pauline Cairns
+Yang Chen
+Ruairidh Grass
+Hannah Green
+Hamish Hoad
+Jo Malo
+Duncan Marjoribanks
+Juan Mata Ruiz
+Paul McPhail Stevenson
+Sarah Murphy
+Katy Preston
+Richard Ramson
+Iain Rogerson
+Davinda Sanders
+Joe Stafford
+Daniel Stewart
+Luis Tejero
+Colin Tindle
+Rachel Westwater
+Zsolt Podoba-Szalai
+Jeff 4
+Jeff 5
+(23 rows)
+
 --9. The cinema has just heard that they will be holding an exclusive midnight showing of 'Guardians of the Galaxy 2'!! Create a new entry in the 'movies' table to reflect this.
 INSERT INTO movies(title, year, show_time) VALUES ('Guardians of the Galaxy 2', 2018, '00:00');
 --SELECT * FROM movies
 
+id |                title                | year | show_time
+----+-------------------------------------+------+-----------
+  1 | Iron Man                            | 2008 | 19:55
+  2 | The Incredible Hulk                 | 2008 | 18:40
+  3 | Iron Man 2                          | 2010 | 21:55
+  4 | Thor                                | 2011 | 16:35
+  5 | Captain America: The First Avenger  | 2011 | 13:25
+  6 | Avengers Assemble                   | 2012 | 22:05
+  7 | Iron Man 3                          | 2013 | 23:10
+  8 | Thor: The Dark World                | 2013 | 12:25
+  9 | Batman Begins                       | 2005 | 14:00
+ 10 | Captain America: The Winter Soldier | 2014 | 14:45
+ 11 | Guardians of the Galaxy             | 2014 | 18:55
+ 12 | Avengers: Age of Ultron             | 2015 | 20:40
+ 13 | Ant-Man                             | 2015 | 13:50
+ 14 | Captain America: Civil War          | 2016 | 21:30
+ 15 | Doctor Strange                      | 2016 | 17:55
+ 16 | Guardians of the Galaxy 2           | 2018 | 00:00
+(16 rows)
+
 --10. The cinema would also like to make the Guardian movies a back to back feature. Update the 'Guardians of the Galaxy' show time from 18:55 to 21:30
 UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
 --SELECT * FROM movies
+
+id |                title                | year | show_time
+----+-------------------------------------+------+-----------
+  1 | Iron Man                            | 2008 | 19:55
+  2 | The Incredible Hulk                 | 2008 | 18:40
+  3 | Iron Man 2                          | 2010 | 21:55
+  4 | Thor                                | 2011 | 16:35
+  5 | Captain America: The First Avenger  | 2011 | 13:25
+  6 | Avengers Assemble                   | 2012 | 22:05
+  7 | Iron Man 3                          | 2013 | 23:10
+  8 | Thor: The Dark World                | 2013 | 12:25
+  9 | Batman Begins                       | 2005 | 14:00
+ 10 | Captain America: The Winter Soldier | 2014 | 14:45
+ 12 | Avengers: Age of Ultron             | 2015 | 20:40
+ 13 | Ant-Man                             | 2015 | 13:50
+ 14 | Captain America: Civil War          | 2016 | 21:30
+ 15 | Doctor Strange                      | 2016 | 17:55
+ 16 | Guardians of the Galaxy 2           | 2018 | 00:00
+ 11 | Guardians of the Galaxy             | 2014 | 21:30
+(16 rows)
 
 --## Extension
 
@@ -99,5 +317,29 @@ UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
 
 DELETE FROM movies WHERE id > 4;
 --SELECT * FROM movies
+
+id |        title        | year | show_time
+----+---------------------+------+-----------
+ 1 | Iron Man            | 2008 | 19:55
+ 2 | The Incredible Hulk | 2008 | 18:40
+ 3 | Iron Man 2          | 2010 | 21:55
+ 4 | Thor                | 2011 | 16:35
+(4 rows)
+
 DELETE FROM movies WHERE year IN ('2013', '2016');
 --SELECT * FROM movies
+
+id |                title                | year | show_time
+----+-------------------------------------+------+-----------
+ 1 | Iron Man                            | 2008 | 19:55
+ 2 | The Incredible Hulk                 | 2008 | 18:40
+ 3 | Iron Man 2                          | 2010 | 21:55
+ 4 | Thor                                | 2011 | 16:35
+ 5 | Captain America: The First Avenger  | 2011 | 13:25
+ 6 | Avengers Assemble                   | 2012 | 22:05
+10 | Captain America: The Winter Soldier | 2014 | 14:45
+12 | Avengers: Age of Ultron             | 2015 | 20:40
+13 | Ant-Man                             | 2015 | 13:50
+16 | Guardians of the Galaxy 2           | 2018 | 00:00
+11 | Guardians of the Galaxy             | 2014 | 21:30
+(11 rows)
