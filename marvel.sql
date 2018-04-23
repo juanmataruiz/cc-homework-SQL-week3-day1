30e45fd5d97fc21a00f1f9bcbcef6fcfa8266d4c
Juan <juanmataruiz84@gmail.com>
2018-04-23 16:53:41 +0100

    all tests pass

--- marvel.sql
+++ marvel.sql
@@ -149,3 +149,36 @@ INSERT INTO attendances (person_id, movie_id) VALUES (12, 2);
 INSERT INTO attendances (person_id, movie_id) VALUES (5, 8);
 INSERT INTO attendances (person_id, movie_id) VALUES (1, 1);
 INSERT INTO attendances (person_id, movie_id) VALUES (21, 4);
+
+--## Questions
+
+--1. Return ALL the data in the 'movies' table.
+SELECT * FROM movies;
+--2. Return ONLY the name column from the 'people' table
+SELECT name FROM people;
+--3.Oops! Someone at CodeClan spelled Davina's name wrong! Change it to reflect the proper spelling (change 'Davinda Sanders' to 'Davina Sanders').
+UPDATE people SET name = 'Davina Sanders' WHERE name = 'Davinda Sanders';
+--4. Return ONLY your name from the 'people' table.
+SELECT name FROM people WHERE name = 'Juan Mata Ruiz';
+--5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
+DELETE FROM movies WHERE title = 'Batman Begins';
+--6. Create a new entry in the 'people' table with the name of one of the instructors.
+INSERT INTO people (name) VALUES ('Sandy');
+--SELECT name FROM people;
+--7. Zsolt, has decided to hijack our movie evening, Remove him from the table of people.
+DELETE FROM people WHERE name = 'Zsolt Podoba-Szalai';
+
+--8. Somehow the list of people includes two people named 'Andrew'. Change these entries to the proper names ('Jeff 4', 'Jeff 5')
+UPDATE people SET name = 'Jeff 4' WHERE name = 'Andrew Laughlin';
+UPDATE people SET name = 'Jeff 5' WHERE name = 'Andrew Smith';
+--SELECT name FROM people;
+
+--9. The cinema has just heard that they will be holding an exclusive midnight showing of 'Guardians of the Galaxy 2'!! Create a new entry in the 'movies' table to reflect this.
+INSERT INTO movies(title, year, show_time) VALUES ('Guardians of the Galaxy 2', 2018, '00:00');
+
+--10. The cinema would also like to make the Guardian movies a back to back feature. Update the 'Guardians of the Galaxy' show time from 18:55 to 21:30
+UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
+
+--## Extension
+
+--1. Research how to delete multiple entries from your table in a single command.
